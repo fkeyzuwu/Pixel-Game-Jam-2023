9e73f946d93bf74b213a15648f69a3b27644b065
fkeyz <fkeyzuwu@gmail.com>
2023-05-02 21:50:39 +0300

    added serpent pathing

--- Assets/Scripts/Serpent/SerpentCharacterController.cs
+++ Assets/Scripts/Serpent/SerpentCharacterController.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class SerpentCharacterController : CharacterBasicController
 {
+    [SerializeField] private Transform waypointObject;
+    private List<Vector3> waypoints = new List<Vector3>();
+    private int currentWaypointIndex = 0;
+    private bool finishedMoving = false;
 
+    private void Awake()
+    {
+        foreach(Transform child in waypointObject) 
+        {
+            waypoints.Add(child.position);
+        }
+    }
+
+    private void Update()
+    {
+        if (finishedMoving) return;
+
+        if(Vector2.Distance(transform.position, waypoints[currentWaypointIndex]) > 0.1f) 
+        {
+            Vector2 direction = waypoints[currentWaypointIndex] - transform.position;
+            Move(direction.normalized);
+        }
+        else 
+        {
+            if(waypoints.Count > currentWaypointIndex + 1) 
+            {
+                currentWaypointIndex++;
+            }
+            else 
+            {
+                finishedMoving = true;
+                IsWalking = false;
+                MoveDelta = Vector2.zero;
+                Rigidbody.velocity = Vector2.zero;
+            }
+        }
+    }
+
+    protected override void Move(Vector2 moveDelta)
+    {
+        IsWalking = moveDelta != Vector2.zero;
+        MoveDelta = moveDelta;
+        Rigidbody.velocity = new Vector2(moveDelta.x * movementSpeed, moveDelta.y * movementSpeed);
+    }
 }
