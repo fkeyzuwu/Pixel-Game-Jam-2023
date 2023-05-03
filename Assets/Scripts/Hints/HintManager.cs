3d6a457e9a6879e3bec69387c6d9675e7dd0ada4
Ran Biderman <bsuro10@gmail.com>
2023-05-03 22:12:42 +0300

    Added cinemachine Added leantween

--- Assets/Scripts/Hints/HintManager.cs
+++ Assets/Scripts/Hints/HintManager.cs
@@ -6,8 +6,6 @@ using UnityEngine.UI;
 
 public class HintManager : MonoBehaviour
 {
-    public HintData movementHint;
-    
     [SerializeField] private TextMeshProUGUI hintText;
     [SerializeField] private Image hintBorder;
 
@@ -18,10 +16,10 @@ public class HintManager : MonoBehaviour
         _hintCanvas = GetComponent<Canvas>();
         _hintCanvas.enabled = false;
         hintText.text = "";
-        /*hintBorder.transform.localScale = Vector3.zero; TODO: Uncomment when tween*/
+        hintBorder.transform.localScale = Vector3.zero;
     }
 
-    IEnumerator ShowHint(HintData hintData)
+    public IEnumerator ShowHint(HintData hintData)
     {
         EnableHint(hintData.text);
         yield return new WaitForSeconds(hintData.durationInSeconds);
@@ -32,25 +30,18 @@ public class HintManager : MonoBehaviour
     {
         hintText.text = hint;
         _hintCanvas.enabled = true;
+        LeanTween.scale(hintBorder.rectTransform, new Vector3(1, 1, 1), 1f).setEase(LeanTweenType.easeOutBounce);
         // TODO: Add LeanTweet with DOScale(new Vector3(1, 1, 1), 1f).SetEase(Ease.OutBounce);
     }
 
     private void DisableHint()
     {
-        /* TODO: Add LeanTweet with DOScale(Vector3.zero, 0.5f).SetEase(Ease.InBounce).OnComplete(() =>
-        {
-            _hintCanvas.enabled = false;
-            hintText.text = "";
-        });*/
-        _hintCanvas.enabled = false;
-        hintText.text = "";
+        LeanTween.scale(hintBorder.rectTransform, Vector3.zero, 0.5f).setEase(LeanTweenType.easeInBounce)
+            .setOnComplete(() =>
+            {
+                _hintCanvas.enabled = false;
+                hintText.text = "";
+            });
     }
-    
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            StartCoroutine(ShowHint(movementHint));
-        }
-    }
+
 }
